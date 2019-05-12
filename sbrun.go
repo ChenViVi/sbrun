58c5bed08f4172220dca6513e21375d1330a9810
chenvivi <932942491@qq.com>
2019-05-12 14:08:07 +0800

    已是成了

--- sbrun.go
+++ sbrun.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"io"
-//	"os"
 	"os/exec"
 	"regexp"
-//	pipe "github.com/b4b4r07/go-pipe"
 )
 
 func main() {
@@ -32,53 +30,16 @@ func run(applicationPropertiesBytes []byte){
 		exec.Command("kill", "-s", "9", pid).Start()
 	}
 	exec.Command( "nohup", "mvn", "spring-boot:run").Start()
-	//os.Exit(1)
 }
 
-//netstat -lnp|grep port
 func GetPid(port string) string{
-	//var pidBytes bytes.Buffer
-	// if err := pipe.Command(&pidBytes,
-	// 	exec.Command("netstat", "-lnp"),
-	// 	exec.Command("grep", port),
-	// ); err != nil {
-	// 	fmt.Println(err)
-	// 	return "";
-	// }
-
-
-
-	// c1 := exec.Command("ls")
-	// c2 := exec.Command("wc", "-l")
-
-	// r, w := io.Pipe() 
-	// c1.Stdout = w
-	// c2.Stdin = r
-
-	// var pidBytes bytes.Buffer
-	// c2.Stdout = &pidBytes
-
-	// c1.Start()
-	// c2.Start()
-	// c1.Wait()
-	// w.Close()
-	// c2.Wait()
-	// pidStr := pidBytes.String()
-	// pidStrRe, _ := regexp.Compile("([1-9]\\d*)/java")
-	// pidStr = pidStrRe.FindString(pidStr)
-	// pidRe, _ := regexp.Compile("[1-9]\\d*")
-	// pid := pidRe.FindString(pidStr)
-
 	c1 := exec.Command("netstat", "-lnp")
 	c2 := exec.Command("grep", port)
-
 	r, w := io.Pipe()
 	c1.Stdout = w
 	c2.Stdin = r
-
 	var pidBytes bytes.Buffer
 	c2.Stdout = &pidBytes
-
 	c1.Start()
 	c2.Start()
 	c1.Wait()
@@ -99,13 +60,4 @@ func GetPort(applicationPropertiesBytes []byte) string{
 	portRe, _ := regexp.Compile("[1-9]\\d*")
 	port := portRe.FindString(portStr)
 	return port
-}
-
-func ExeCommand(wait bool, commandName string, arg ...string) {
-	cmd := exec.Command(commandName, arg...)
-	fmt.Println(cmd.Args)
-	cmd.Start()
-	if wait {
-		cmd.Wait()
-	}
 }
